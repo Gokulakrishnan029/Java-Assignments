48a73bc3b8d51ca701ced8762b6777a1c2a7458b
Gokulakrishnan029 <gokulmastro29@gmail.com>
2024-04-03 16:31:08 +0530

    Collections and generics

---
+++ Collections and generics/GenericUtils.java
@@ -0,0 +1,24 @@
+package CollectionsAndGenerics;
+
+public class GenericUtils {
+	public static <T> T[] swap(T[] list, int firstPos, int secondPos) {
+		if (firstPos < 0 || firstPos >= list.length || secondPos < 0 || secondPos >= list.length) {
+			throw new IndexOutOfBoundsException("Invalid index. Make sure given values are in range.");
+		}
+
+		T temp = list[firstPos];
+		list[firstPos] = list[secondPos];
+		list[secondPos] = temp;
+
+		return list;
+	}
+
+	public static void main(String[] args) {
+		Integer[] numbers = { 1, 2, 3, 4, 5 };
+		swap(numbers, 1, 3); 
+		for (Integer num : numbers) {
+			System.out.println(num + " ");
+		}
+		
+	}
+}
